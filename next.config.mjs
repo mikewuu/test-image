bbf4593f016de08d155776b091367fef540e8a89
Mike Wu <hello@mikewu.dev>
2024-05-01 15:23:16 +0900

    set image pattern

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: 'connect.getseam.com',
+      },
+    ],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
